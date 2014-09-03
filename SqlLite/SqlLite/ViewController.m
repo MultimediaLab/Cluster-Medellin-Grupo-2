c59cdace2163c70f48820c305fd24c4d204a3a4c
Giovanny Suarez <suarezkrac@gmail.com>
2014-09-03 08:20:50 -0500

    sql
    sql

--- SqlLite/SqlLite/ViewController.m
+++ SqlLite/SqlLite/ViewController.m
@@ -27,9 +27,10 @@
     dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     docsDir = [dirPaths objectAtIndex:0];
     
-    NSLog(@"%@", docsDir);
+  
     
     databasePath = [[NSString alloc] initWithString:[docsDir stringByAppendingPathComponent:@"contacts.db"]];
+      NSLog(@"%@", databasePath);
     NSFileManager * fm = [[NSFileManager alloc] init];
     
     if ([fm fileExistsAtPath:databasePath]==NO) {
@@ -59,9 +60,67 @@
 }
 
 - (IBAction)saveData:(id)sender {
+    if (_empNoTxt.text.length > 0 && _empNameTxt.text.length>0 && _empAgeTxt.text.length>0 && _empAdressTxt.text.length>0) {
+        sqlite3_stmt * statement;
+        const char * db = [databasePath UTF8String];
+        if (sqlite3_open(db, &contactDB)== SQLITE_OK) {
+            //Acceder a la BD
+            NSString * insert = [[NSString alloc] initWithFormat:@"INSERT INTO CONTACTS (EMP_NO, EMP_NAME, EMP_AGE, EMP_ADRESSS) VALUES (\"%@\",\"%@\",\"%@\",\"%@\")", _empNoTxt.text, _empNameTxt.text, _empAgeTxt.text, _empAdressTxt.text];
+            const char * insert_sql = [insert UTF8String];
+
+            sqlite3_prepare_v2(contactDB, insert_sql, -1, &statement, NULL);
+            if (sqlite3_step(statement)==SQLITE_DONE) {
+                _status.text = @"Registro Almacenado con Exito!!";
+                _empNoTxt.text = @"";
+                _empNameTxt.text = @"";
+                _empAgeTxt.text = @"";
+                _empAdressTxt.text = @"";
+            }else
+            {
+                _status.text = @"Error al almacenar registro";
+            }
+            sqlite3_finalize(statement);
+            sqlite3_close(contactDB);
+        }
+        else{
+            _status.text = @"No se pudo acceder a la bd";
+        }
+    }else{
+        _status.text = @"Datos incompletos";
+    }
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    for (UIView * view in self.view.subviews) {
+        [view resignFirstResponder];
+    }
 }
 
 - (IBAction)readData:(id)sender {
+    sqlite3_stmt * statement;
+    const char * db = [databasePath UTF8String];
+    if (sqlite3_open(db, &contactDB)== SQLITE_OK) {
+        NSString * select = [NSString stringWithFormat:@"SELECT EMP_NAME, EMP_AGE, EMP_ADRESSS FROM CONTACTS WHERE EMP_NO = \"%@\"", self.empNoTxt.text];
+        const char * select_sql = [select UTF8String];
+        if (sqlite3_prepare_v2(contactDB, select_sql, -1, &statement, NULL)== SQLITE_OK) {
+            if (sqlite3_step(statement)==SQLITE_ROW) {
+                _status.text = @"Registro encontrado";
+                _empNameTxt.text = [NSString stringWithFormat:@"%s", sqlite3_column_text(statement, 0)];
+                _empAgeTxt.text = [NSString stringWithFormat:@"%s", sqlite3_column_text(statement, 1)];
+                _empAdressTxt.text = [NSString stringWithFormat:@"%s", sqlite3_column_text(statement, 2)];
+            }
+            else{
+                _status.text = @"Registro no encontrado";
+            }
+            sqlite3_finalize(statement);
+        }
+        else{
+            _status.text=@"Error en el Query";
+        }
+        sqlite3_close(contactDB);
+    }else{
+        _status.text = @"No se pudo acceder a la bd";
+    }
+    
 }
 
 - (IBAction)updateData:(id)sender {

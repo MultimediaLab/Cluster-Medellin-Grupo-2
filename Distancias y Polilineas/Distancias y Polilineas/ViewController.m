9bb20b083a7f7dfa0cdd5a670626fbf658388248
Giovanny Suarez <suarezkrac@gmail.com>
2014-09-08 17:21:15 -0500

    nuevo
    nuevo

---
+++ Distancias y Polilineas/Distancias y Polilineas/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Distancias y Polilineas
+//
+//  Created by Profesor on 8/09/14.
+//  Copyright (c) 2014 Profesor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Politecnico : 6.209659,-75.577521
+    // Mi Casa: 6.217584,-75.5685735
+    // Punto Medio: 6.2135973,-75.5728505
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
